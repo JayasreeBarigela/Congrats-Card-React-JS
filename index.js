dca409c567aa78bca29042cc9cb0d7b97a77f33e
root <Jayasreeb13@gmail.com>
2022-02-04 09:20:20 +0000

    Congrats Card

---
+++ index.js
@@ -0,0 +1,22 @@
+const element = (
+  // Write your code here.
+  <div className="bg">
+    <h1 className="heading"> Congratulations</h1>
+    <div class="card-container">
+      <img
+        src="https://res.cloudinary.com/dxd0etrag/image/upload/v1635570216/jayasree_rzu79y.jpg"
+        class="image"
+      />
+      <h1 class="card-heading">Jayasree B</h1>
+      <p class="card-text">
+        Jayasree Institute of computer education and technology, Nizamabad.
+      </p>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png"
+        class="image"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
